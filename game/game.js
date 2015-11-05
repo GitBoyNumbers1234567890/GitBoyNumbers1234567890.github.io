f7de6a2038d422c09b9c598afb05900cfe746869
GitBoyNumbers1234567890 <kwaiyuk.nam@online.island.edu.hk>
2015-11-05 15:53:06 +0800

    Progress

---
+++ game/game.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#start").click(function() {
+    	$("#start").hide();
+		$("#one").css("background-image", "url('http://info.cassidyturley.com/Portals/163401/images/Industrial%20warehouse%20inside.JPG')");
+		$("#menu").hide();
+		$("#m1").css("display", "block");
+	});
+}); 
